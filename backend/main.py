4a44d994bc6aff57959adad7c484f3477fbb8adb
Oscar Umana <oscarumana1021@gmail.com>
2024-03-31 02:59:22 -0400

    remove comments

--- backend/main.py
+++ backend/main.py
@@ -35,11 +35,9 @@ async def feedback(body: Item):
     for question in wrong_questions:
         #print(question.selected_answer)
         additional_info = additional(question)
-        response_info.append(additional_info.text)
-    return json.loads(''.join(response_info))    
-        
-    
-
+        print(additional_info)
+        response_info.append(additional_info)
+    return response_info    
 
 @app.get('/questions/{category}')
 def get_questions(category):
@@ -55,40 +53,13 @@ def get_questions(category):
     # Generate responses (Replace this with your own generate function)
     # Assuming responses is a list of generated responses
     responses = generate(file_contents)  # Call your generate function with file contents
-
-    # Print each response
-    # for response in responses:
-    #     print(response.text, end="")
-
-    # # Return information about the file
-    # return {"filename": filename}
     
     response_texts = []
     for response in responses:
         response_texts.append(response.text)
-        # print(response.text)
-    # print(len(responses))
-    # return responses.to_dict()
     
     # Return the response as JSON
     return json.loads(''.join(response_texts))
-    # json_string = json.dumps(my_dict)
-    # return json_string
-
-# #how to receive query parameters with fast api
-# @app.post("/upload/")
-# async def upload_file(file: UploadFile = File(...)):
-#     # Save the uploaded file to a specific location
-#     with open(f"uploaded_files/{file.filename}", "wb") as buffer:
-#         buffer.write(await file.read())
-    
-#     # prompt = f"Based on the following text, generate 10 multiple choice questions{contents}"
-#     responses = generate(buffer.read())
-#     for response in responses:
-#         print(response.text, end="")
-        
-#     return {"filename": file.filename}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
